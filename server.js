c461585f68a7bb28e679fab28d44a199ca0c173c
tober65 <timothy.ober@gmail.com>
2020-08-13 10:49:24 -0700

    Added DB info

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://tober65:password1@ds121089.mlab.com:21089/heroku_5qkmsn6p");
 
 // Start the API server
 app.listen(PORT, function() {
